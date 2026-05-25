008963b0853cee5375ff42b0a42cd50abb79eb1a
Astramaniac <omgspamforme@gmail.com>
2026-05-25 11:39:54 +0530

    Finally a proper file format for downstream use.

--- features/coordinates.R
+++ features/coordinates.R
@@ -1,6 +1,7 @@
 # =============================================================================
-# Append geometric information to the GWL time series data by a left join on station_code
-# with the GWL stations shapefile. Writes new shapefiles!
+# Append geometric information to the GWL time series data
+# by a left join on station_code with the GWL stations shapefile.
+# Writes new shapefiles!
 # =============================================================================
 library(sf)
 library(readr)
@@ -8,52 +9,49 @@ library(dplyr)
 library(lubridate)
 library(rnaturalearth)
 library(ggplot2)
+library(arrow)
 
-gwl_stations <- read_sf("data/shp/GWL_Stations_Within_bounds/GWL_Stations_Within_bounds.shp")
-cgwb <- read_csv("data/wris_webscrape/CGWB.csv")
+gwl_stations <- read_sf("data/shp/GWL_Station/GroundwaterLevel_Station.shp")
+gwl_stations <- read_sf("data/shp/GWL_Stations_Within_bounds/GWL_Stations_Within_bounds.shp") # fixed file
+cgwb <- read_csv("data/wris/CGWB.csv")
 
 # convert to Date, filter relevant stations and create same name for join
-cgwb <- cgwb %>%
-  mutate(timestamp = ym(timestamp))
 cgwb_stations <- gwl_stations %>%
   filter(agency_nam == "CGWB") %>%
   rename(station_code = station_co)
+cgwb <- cgwb %>%
+  mutate(timestamp = ym(timestamp))
 
 cgwb <- cgwb %>%
   left_join(cgwb_stations, by = "station_code")
+# attributes are not present on left join, make sf object again
+cgwb <- st_as_sf(cgwb, crs = st_crs(gwl_stations), sf_column_name = 'geometry')
 
 # confirm presence of NAs in lat/long (or lack thereof)
 sum(is.na(cgwb$lat))
 sum(is.na(cgwb$long))
 
+# FOR WITHIN BOUNDS SHAPEFILE
+# extract readings with missing coordinates
 cgwb_missing <- cgwb %>%
   filter(is.na(lat) | is.na(long))
-
-cgwb_missing_stations <- cgwb_missing %>%
-  distinct(station_code, .keep_all = TRUE)
-
-# optional: plot the region covered by the stations
-india_map <- map_data("world") %>%
-  filter(region == "India")
-plot <- ggplot() +
-  geom_polygon(data = india_map,
-               aes(x = long, y = lat, group = group),
-               color = "black", alpha = 0, linewidth = 0.1) +
-  geom_sf(data = st_geometry(cgwb_stations))
-  # geom_sf(mapping = aes(fill = state_name), data = cgwb_stations, linewidth = 0.2)
-ggsave("cgwb.png", plot = plot, dpi = 500, limitsize = FALSE, units = "px")
+# CGWB: lost 6219 readings. A 0.05% loss. Sure thing. We'll roll with it.
 
 
-# addendum: realise something's wrong with the plot!
-# Find the stations that lie out of any reasonable boundary
-india_region <- read_sf("data/shp/IND_adm/IND_adm0.shp")
-cgwb_stations <- cgwb_stations %>% st_set_crs(4326) %>% st_transform(4326)
-out_of_bounds <- cgwb_stations %>%
-  filter(st_within(geometry, india_region, sparse = FALSE))
+# write_sf(cgwb, "data/shp/CGWB.shp")
+# # Warnings generated, these suck. Trying another file format.
+# #   Field names abbreviated for ESRI Shapefile driver
+# #   GDAL Message 1: One or several characters couldn't be converted correctly from UTF-8 to ISO-8859-1.
+# #   GDAL Message 1: 2GB file size limit reached for data/shp/CGWB.dbf.
+# # Absolutely lame. Completely unnecessary design flaw. Screw this.
 
+# # Take Two: Use GeoParquet instead.
+# write_parquet(cgwb, "data/gpkg/CGWB.parquet")
+# cgwb2 <- read_parquet("data/gpkg/CGWB.parquet")
+# # You lose spatial characteristics. Try another file format.
 
-
-png("whatever.png",
-    width = 1500, height = 1500, units = "px")
-plot(st_geometry(gwl_stations))
-dev.off()
+# Take Three: Use gpkg instead.
+st_write(cgwb, "data/gpkg/CGWB.gpkg", append = FALSE)
+# cgwb2 <- st_read("data/gpkg/CGWB.gpkg")
+# st_geometry(cgwb2)
+# Success!
